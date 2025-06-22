fcd7b78993ebf5f8c01e6469212620b903bacfc1
JoSantana0005 <joseasantana05@gmail.com>
2025-06-22 11:59:55 -0400

    feat: :hammer: add the models for finance

--- Backend/Models/Finance.mjs
+++ Backend/Models/Finance.mjs
@@ -1,4 +1,141 @@
 import { connection } from "./db/connection.mjs";
 export class ModelsFinance {
     // Obtener todas las finanzas
+    static async getAllFinances(){
+        const [ Finances ] = await connection.query("SELECT * FROM finances");
+        if(Finances.length > 0){
+            console.log("Finanzas obtenidas correctamente");
+            return Finances;
+        }
+        else{
+            console.log("No se encontraron finanzas");
+            return [];
+        }
+    }
+
+    // Obtener las finanzas de un usuario
+    static async getByUserID({id_user}){
+        if(!id_user) return { error: "ID de usuario no proporcionado" };
+        const [ Finances ] = await connection.query("SELECT * FROM register_finance WHERE id_user = ?", [id_user]);
+        if(Finances.length > 0){
+            console.log("Finanzas obtenidas correctamente");
+            return Finances;
+        }
+        else{
+            console.log("No se encontraron finanzas para el usuario con ID:", id_user);
+            return [];
+        }
+    }
+
+    // Obtener las finanzas por su cantidad
+    static async getByAmount({amount_finance}){
+        if(!amount_finance) return { error: "Cantidad no propocionada" };
+        const [ Finances ] = await connection.query("SELECT * FROM register_finance WHERE amount_finance = ?", [amount_finance]);
+        if(Finances.length > 0){
+            console.log("Finanzas obtenidas correctamente");
+            return Finances;
+        }
+        else{
+            console.log("No se encontraron finanzas con la cantidad:", amount_user);
+            return [];
+        }
+    }
+
+    // Obtener las finanzas por el tipo de finanza
+    static async getByType({ type_finance }){
+        if(!type_finance) return { error: "Tipo de finanza no proporcionado"};
+        const [ Finances ] = await connection.query("SELECT * FROM register_finance WHERE type_finance = ?", [type_finance]);
+        if(Finances.length > 0){
+            console.log("Finanzas obtenidas correctamente");
+            return Finances;
+        }
+        else{
+            console.log("No se encontraron finanzas con el tipo:", type_finance);
+            return [];
+        }
+    }
+
+    // Obtener la categoria de la finanza
+    static async getByCategory({ category_finance }){
+        if(!category_finance) return { error: "Categoria de finanza no proporcionada" };
+        const [ Finances ] = await connection.query("SELECT * FROM register_finance WHERE category_finance = ?", [category_finance]);
+        if(Finances.length > 0){
+            console.log("Finanzas obtenidas correctamente");
+            return Finances;
+        }
+        else{
+            console.log("No se encontraron finanzas con la categoria:", category_finance);
+            return [];
+        }
+    }
+
+    // Crear una nueva finanza
+    static async createFinance({ id_user, finance}){
+        if(!id_user || !finance) return { error: "ID de usuario o finanza no proporcionados" };
+        // Se verifica si el usuario esta loguado
+        const [ user ] = await connection.query("SELECT * FROM login_users WHERE id_user = ?", [id_user]);
+        if(user.length > 0){
+            console.log("Usuario encontrado, creando finanza...");
+            const userID = user[0].id_user;
+            const { name_finance, description_finance, date_finance, type_finance, category_finance, amount_finance } = finance;
+            // Se inserta la nueva finanza en la base de datos
+            const [ financeCreated ] = await connection.query(
+                `INSERT INTO register_finance (id_user, name_finance ,description_finance, date_finance, type_finance, category_finance, amount_finance)
+                VALUES (?, ? ,?, ?, ?, ?, ?)`,
+                [userID, name_finance, description_finance, date_finance, type_finance, category_finance, amount_finance]
+            )
+            if(financeCreated.affectedRows > 0){
+                console.log("Finanza creada correctamente");
+                return financeCreated;
+            }
+            else{
+                console.log("Error al crear la finanza");
+                return { error: "Error al crear la finanza" };
+            }
+        }
+    }
+
+    // Eliminar una finanza por el nombre
+    static async deleteFinanceByName({ id_user, name_finance }){
+        if(!id_user || !name_finance) return { error: "ID de usuario o nombre de finanza no proporcionados"};
+        // Se verifica si el usuario esta logueado
+        const [ user ] = await connection.query("SELECT * FROM login_users WHERE id_user = ?", [id_user]);
+        if(user.length > 0){
+            console.log("Usuario encontrado, eliminando Finanza...");
+            const userID = user[0].id_user;
+            // Se elimina la finanza de la base de datos
+            const [ financeDeleted ] = await connection.query(
+                `DELETE FROM register_finance WHERE id_user = ? AND name_finance = ?`,
+                [userID, name_finance]
+            )
+            if(financeDeleted.affectedRows > 0){
+                console.log("Finanza eliminada correctamente");
+                return financeDeleted;
+            }
+            else{
+                console.log("Error al eliminar la finanza o no se encontró la finanza");
+                return { error: "Error al eliminar la finanza o no se encontró la finanza" };
+            }
+        }
+    }
+
+    // Actualizar una finanza por su ID
+    static async updateFinanceByID({ id_finance,finance }){
+        if(!id_finance || !finance) return { error: "ID de finanza o finanza no proporcionados" };
+        // Se actualiza la finanza en la base de datos
+        const { name_finance, description_finance, date_finance, type_finance, category_finance, amount_finance } = finance;
+        const [ financeUpdated ] = await connection.query(
+            `UPDATE register_finance SET name_finance = ?, description_finance = ?, date_finance = ?, type_finance = ?, category_finance = ?, amount_finance = ? WHERE id_finance = ?`,
+            [name_finance, description_finance, date_finance, type_finance, category_finance, amount_finance, id_finance]
+        )
+        if(financeUpdated.affectedRows > 0){
+            console.log("Finanza actualizada correctamente");
+            return financeUpdated;
+        }
+        else{
+            console.log("Error al actualizar la finanza o no se encontró la finanza");
+            return { error: "Error al actualizar la finanza o no se encontró la finanza" };
+        }
+    }
+    
 }
