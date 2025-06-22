f1bd8a297fc3dfacd0a7d7655d60383a691cf1b1
JoSantana0005 <joseasantana05@gmail.com>
2025-06-22 12:28:08 -0400

    feat: :construction: Add the finance Routes

--- Backend/Routes/Finance.mjs
+++ Backend/Routes/Finance.mjs
@@ -8,3 +8,18 @@ export const RoutesFinance = router;
 
 // GET
 // Obtener todas las finanzas
+RoutesFinance.get('/', controllerfinance.getAllFinances);
+// Obtener las finanzas de un usuario
+RoutesFinance.get('/user/:id_user', controllerfinance.getByUserID);
+// Obtener las finanzas por su cantidad
+RoutesFinance.get('/amount/:amount_finance', controllerfinance.getByAmount);
+// Obtener las finanzas por el tipo de finanza
+RoutesFinance.get('/type/:type_finance', controllerfinance.getByType);
+// Obtener la categoria de la finanza
+RoutesFinance.get('/category/:category_finance', controllerfinance.getByCategory);
+// Crear una nueva finanza
+RoutesFinance.post('/create/:id_user', controllerfinance.createFinance);
+// Actualizar una finanza
+RoutesFinance.put('/update/:id_finance', controllerfinance.updateFinanceByID);
+// Eliminar una finanza
+RoutesFinance.delete('/deleted/:id_user/name/:name_finance', controllerfinance.deleteFinanceByName);
