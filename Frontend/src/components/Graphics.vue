1a15fc6f723813d584f9798b65ffa46f4c2b84f6
JoSantana0005 <joseasantana05@gmail.com>
2025-06-27 13:19:06 -0400

    build: :arrow_down: add canvas-confetti and chart.js

--- Frontend/src/components/Graphics.vue
+++ Frontend/src/components/Graphics.vue
@@ -2,13 +2,16 @@
     <div class="w-full h-11/12 flex flex-col gap-2">
         <div class="border-2 border-gray-500 rounded-2xl w-full h-2/5 flex items-center justify-around">
             <div class="w-1/2 h-full border-r-2 border-gray-500 flex items-center justify-center">
+                <canvas id="cicleChart"></canvas>
                 <p>Grafica de Circulo</p>
             </div>
             <div class="w-1/2 h-full flex items-center justify-center">
+                <canvas id="cicleChart2"></canvas>
                 <p>Grafico de Circulo</p>
             </div>
         </div>
         <div class="border-2 border-gray-500 rounded-2xl w-full h-3/5 flex items-center justify-end px-20">
+            <canvas id="barChart"></canvas>
             <p>Grafica de Barras</p>
         </div>
     </div>
